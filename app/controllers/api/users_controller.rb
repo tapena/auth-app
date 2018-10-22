40402979a13fee8f82b519665729882cf238b84d
Tatiana Pena <Tatiana.c.pena1@gmail.com>
2018-10-22 14:29:12 -0500

    10.22.18

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,16 +1,15 @@
 class Api::UsersController < ApplicationController
   def create
     user = User.new(
-    name: params[:name],
-    email: params[:email]
-    #password: params[:password]
-    #password_confirmation: params[:password_confirmation]
-    )
-
+                    name: params[:name],
+                    email: params[:email],
+                    password: params[:password],
+                    password_confirmation: params[:password_confirmation]
+                    )
     if user.save
-      render json: {message: "user successfully created"}, status: :created  
+      render json: {message: 'User created successfully'}, status: :created
     else
-      render json: {errors: user.erros.full_messages}, status: :bad_request
-    end  
+      render json: {errors: user.errors.full_messages}, status: :bad_request
+    end
   end
 end
